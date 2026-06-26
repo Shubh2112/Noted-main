f7990d1722f0298822f0ce36777015fb1cf7a63c
Shubh2112 <shubhamdaksh2009@gmail.com>
2026-06-26 14:37:10 +0530

    Update PDF.js worker source URL to version 3.11.174

--- pdf.js
+++ pdf.js
@@ -6,7 +6,7 @@
 "use strict";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.js";
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
 
 // ==========================================
 //  STATE
