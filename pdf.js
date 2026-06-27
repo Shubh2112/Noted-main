21479fa243c13bb5aebbe8b9642a855c1ca0da0a
Shubh2112 <shubhamdaksh2009@gmail.com>
2026-06-27 08:46:50 +0530

    Update PDF_URL to include sharing parameters

--- pdf.js
+++ pdf.js
@@ -1,8 +1,7 @@
-const PDF_URL =
-"https://drive.google.com/file/d/1ePQYKfgBU5SNODrPFRuV6uiLTVo2Wpr8/view";
+const PDF_URL = "https://drive.google.com/file/d/1ePQYKfgBU5SNODrPFRuV6uiLTVo2Wpr8/view?usp=sharing";
 
 const openBtn = document.getElementById("openBtn");
 
 openBtn.addEventListener("click", () => {
-    window.location.href = PDF_URL;
+    window.open(PDF_URL, "_blank");
 });
