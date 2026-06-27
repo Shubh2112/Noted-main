0ac42dca1d55060d2ca2e1d9ebe84acb7d3ed2c7
Shubh122009 <shubhamdaksh2009@gmail.com>
2026-06-27 09:38:58 +0530

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,6 +1,6 @@
 document.getElementById("openBtn").addEventListener("click", () => {
 
     window.location.href =
-    "https://drive.google.com/file/d/1ePQYKfgBU5SNODrPFRuV6uiLTVo2Wpr8/preview";
+    "https://drive.google.com/file/d/1ePQYKfgBU5SNODrPFRuV6uiLTVo2Wpr8/view?usp=drivesdk";
 
 });
