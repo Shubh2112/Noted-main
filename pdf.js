ab463495fcf1c8a31cc56631eda7566508b1b38a
Shubh2112 <shubhamdaksh2009@gmail.com>
2026-06-27 08:37:49 +0530

    Change PDF link behavior on button click
    Updated the event listener to redirect instead of opening a new tab.

--- pdf.js
+++ pdf.js
@@ -1,11 +1,8 @@
 const PDF_URL =
 "https://drive.google.com/file/d/1ePQYKfgBU5SNODrPFRuV6uiLTVo2Wpr8/preview";
 
-const pdfLink = document.getElementById("pdfLink");
 const openBtn = document.getElementById("openBtn");
 
-pdfLink.href = PDF_URL;
-
 openBtn.addEventListener("click", () => {
-    window.open(PDF_URL, "_blank");
+    window.location.href = PDF_URL;
 });
